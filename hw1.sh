b2f0fa89c3807d9ffee723af5563f777b0cbc52d
dshih918 <59632350+dshih918@users.noreply.github.com>
2020-06-08 23:18:59 -0700

    Update hw1.sh

--- hw1.sh
+++ hw1.sh
@@ -6,3 +6,5 @@ cut -d',' -f18 flightdelay2007.csv | sort | uniq -c | sort -n | tail -3 |csvlook
 #question 1a
 cut -d',' -f15,17 flightdelay2007.csv | grep SFO | head -3 | csvlook
 cut -d',' -f15,17 flightdelay2007.csv | grep SFO | head -3 > first3sfo.csv
+
+#david shih
